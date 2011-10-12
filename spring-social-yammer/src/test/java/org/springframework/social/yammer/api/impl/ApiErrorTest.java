98823220b97c6967cfb596db3b7e737bea0a1fa6
Morten Andersen-Gott <morten.andersengott@gmail.com>
2011-10-12 20:21:15 +0200

    fixed failing test

--- spring-social-yammer/src/test/java/org/springframework/social/yammer/api/impl/ApiErrorTest.java
+++ spring-social-yammer/src/test/java/org/springframework/social/yammer/api/impl/ApiErrorTest.java
@@ -5,24 +5,27 @@ import static org.springframework.social.test.client.RequestMatchers.method;
 import static org.springframework.social.test.client.RequestMatchers.requestTo;
 import static org.springframework.social.test.client.ResponseCreators.withResponse;
 
+import java.io.IOException;
+
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.social.NotAuthorizedException;
 import org.springframework.social.RateLimitExceededException;
 
 public class ApiErrorTest extends AbstractYammerApiTest{
 
 	@Test(expected=RateLimitExceededException.class)
-	public void testRateLimitExceeded(){
+	public void testRateLimitExceeded() throws IOException{
 		responseHeaders.setContentType(MediaType.APPLICATION_JSON);
 		mockServer.expect(requestTo("https://www.yammer.com/api/v1/users.json?page=1&reverse=false"))
 				.andExpect(method(GET))
-				.andRespond(withResponse(jsonResource("rate-limit-error.json"), responseHeaders, HttpStatus.valueOf(401),""));
+				.andRespond(withResponse(jsonResource("rate-limit-error"), responseHeaders, HttpStatus.valueOf(401),""));
 		yammerTemplate.userOperations().getUsers(1);		
 	}
 	
-	@Test
-	public void test(){
+	@Test(expected=NotAuthorizedException.class)
+	public void testNotAutorizedException(){
 		responseHeaders.setContentType(MediaType.APPLICATION_JSON);
 		mockServer.expect(requestTo("https://www.yammer.com/api/v1/users.json?page=1&reverse=false"))
 		.andExpect(method(GET))
