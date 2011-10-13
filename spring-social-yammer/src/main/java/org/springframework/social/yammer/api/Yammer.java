ff6451c980078858a96fa363c1c664ff1d98ff0e
Morten Andersen-Gott <morten.andersengott@gmail.com>
2011-10-13 14:58:08 +0200

    Javadoc on Yammer interface

--- spring-social-yammer/src/main/java/org/springframework/social/yammer/api/Yammer.java
+++ spring-social-yammer/src/main/java/org/springframework/social/yammer/api/Yammer.java
@@ -2,18 +2,39 @@ package org.springframework.social.yammer.api;
 
 public interface Yammer{
 
+	/**
+	 * Returns the portion of the API containing the Thread operations
+	 */
 	ThreadOperations threadOperations();
 
+	/**
+	 * Returns the portion of the API containing the Subscription (follow) operations
+	 */
 	SubscriptionOperations subscriptionOperations();
 
+	/**
+	 * Returns the portion of the API containing the Topic (tags) operations
+	 */
 	TopicOperations topicOperations();
 
+	/**
+	 * Returns the portion of the API containing the Search operations
+	 */
 	SearchOperations searchOperations();
 
+	/**
+	 * Returns the portion of the API containing the Group operations
+	 */
 	GroupOperations groupOperations();
 
+	/**
+	 * Returns the portion of the API containing the Message operations
+	 */
 	MessageOperations messageOperations();
 
+	/**
+	 * Returns the portion of the API containing the User operations
+	 */
 	UserOperations userOperations();
 
 }
